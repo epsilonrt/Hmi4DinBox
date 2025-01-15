4531d9122d54bbd572eb1d78e01ddb15d3410da8
epsilonrt <epsilonrt@outlook.fr>
2025-01-15 19:30:56 +0100

    Change license to BSD 3-Clause

--- src/Hmi4DinBox.cpp
+++ src/Hmi4DinBox.cpp
@@ -1,19 +1,9 @@
-/* Copyright © 2018 Pascal JEAN, All rights reserved.
- * This file is part of the Hmi4DinBox Library.
- *
- * The Hmi4DinBox Library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * The Hmi4DinBox Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the Hmi4DinBox Library; if not, see <http://www.gnu.org/licenses/>.
- */
+/*
+   Copyright (c) 2018-2023, Pascal JEAN aka epsilonrt
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause
+*/
 #include "Hmi4DinBox.h"
 #include "version.h"
 
--- src/WireHmi.cpp
+++ src/WireHmi.cpp
@@ -1,19 +1,9 @@
-/* Copyright © 2018 Pascal JEAN, All rights reserved.
- * This file is part of the WireHmi Library.
- *
- * The WireHmi Library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * The WireHmi Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the WireHmi Library; if not, see <http://www.gnu.org/licenses/>.
- */
+/*
+   Copyright (c) 2018-2023, Pascal JEAN aka epsilonrt
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause
+*/
 #include "WireHmi.h"
 
 // -----------------------------------------------------------------------------
