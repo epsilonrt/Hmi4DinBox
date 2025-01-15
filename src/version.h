c530487054b4f5010e3344b841fbb1144f11baad
epsilonrt <epsilonrt@outlook.fr>
2025-01-15 19:32:04 +0100

    Update library version to 1.1.0

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
-#define VERSION "1.0-0"
-#define VERSION_SHORT "1.0.0"
-#define VERSION_TINY "1.0"
+#define VERSION "1.1-0"
+#define VERSION_SHORT "1.1.0"
+#define VERSION_TINY "1.1"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
